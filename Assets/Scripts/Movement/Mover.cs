974908a8ac45472d2a51ea9cf1d4e0eba5de4187
Tom Paton <thomas_paton@hotmail.co.uk>
2021-01-12 07:55:15 +0000

    Updates

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        NavMeshAgent navMeshAgent;
+        Health health;
+
+        private void Start() 
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();   
+            health = GetComponent<Health>();
+        }
+
+        void Update()
+        {
+            navMeshAgent.enabled = !health.IsDead();
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel() 
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+    }
+}
